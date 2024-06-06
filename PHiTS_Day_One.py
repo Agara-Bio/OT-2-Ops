c00330659afa934d1729088a4a5e629a826fab90
rishiwahi <69774834+rishiwahi@users.noreply.github.com>
2024-06-06 16:02:13 -0400

    Made 500ul transfer p300 friendly

--- PHiTS_Day_One.py
+++ PHiTS_Day_One.py
@@ -9,9 +9,11 @@ metadata = {
 }
 """ 
 Phage amplification (day 1) 
+2.2 working volume
 a. Distribute a maximum of 94 samples (1.5 mL) in a deep-well plate (no. 1) with pierceable sealing tape (e.g., Z722529-50EA; Excel Scientific, Victorville, CA). 
 To each of the 96 wells add:
-    90uL CaCl2 (0.25 M) and MgCl2 (0.25 M), final concentration 10 mM.
+    90uL CaCl2 (0.25 M) and
+    MgCl2 (0.25 M), final concentration 10 mM.
     110uL ON host culture, final concentration 5% v/v.
     500uL host medium (concentration · 4.4), final concentration · 1. During addition of medium, carefully pipette up and down a few times to mix.
 Sterilized water (1.5 mL) is added as negative amplification controls to wells D6 and E6.
@@ -24,10 +26,10 @@ Close the well plate and incubate ON on a shaker (200 rpm).
 def run(PHiTS_1: protocol_api.ProtocolContext):
     
     # Labware
-    deep_well_plate = protocol.load_labware('nest_96_wellplate_2ml_deep', 1)
+    deep_well_plate = protocol.load_labware('nest_96_wellplate_2ml_deep', 6)
     tiprack_200ul = protocol.load_labware('opentrons_96_tiprack_300ul', 2)
-    tiprack_20ul = protocol.load_labware('opentrons_96_tiprack_20ul', 3)
-    reservoir = protocol.load_labware('usascientific_12_reservoir_22ml', 4)
+    tiprack_20ul = protocol.load_labware('opentrons_96_tiprack_20ul', 1)
+    reservoir = protocol.load_labware('usascientific_12_reservoir_22ml', 3)
     
     # Pipettes
     p300 = protocol.load_instrument('p300_single_gen2', 'right', tip_racks=[tiprack_200ul])
@@ -38,7 +40,7 @@ def run(PHiTS_1: protocol_api.ProtocolContext):
     on_host_culture = reservoir.wells_by_name()['A2']
     host_medium = reservoir.wells_by_name()['A3']
     
-    # Step 1: Add 90uL CaCl2 and MgCl2 to each well
+    # Step 1: Add 90uL CaCl2 and MgCl2 to each well // need to split this step
     p300.pick_up_tip()
     for well in deep_well_plate.wells():
         p300.aspirate(90, cacl2_mgcl2)
@@ -53,12 +55,15 @@ def run(PHiTS_1: protocol_api.ProtocolContext):
     p300.drop_tip()
     
     # Step 3: Add 500uL host medium to each well, mixing in the process
-    p1000.pick_up_tip()
+    p300.pick_up_tip()
     for well in deep_well_plate.wells():
-        p1000.aspirate(500, host_medium)
-        p1000.dispense(500, well)
-        p1000.mix(3, 300, well)  # Mix by pipetting up and down 3 times
-    p1000.drop_tip()
+        i = 0
+        while i < 2
+            p300.aspirate(250, host_medium)
+            p300.dispense(250, well)
+            i += 1
+        p300.mix(3, 400, well)  # Mix by pipetting up and down 3 times
+    p300.drop_tip()
     
     # Incubation step (simulated)
     protocol.comment('Incubate the well plate ON on a shaker at 200 rpm.')
